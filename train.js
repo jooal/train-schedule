2468528bef0e3fd699b26c37d2858483a3a313c1
Alex Joo <alexjoo93@gmail.com>
2018-09-14 09:59:40 -0400

    push

--- train.js
+++ train.js
@@ -1,4 +1,4 @@
-var now = new Date();
+// var now = new Date();
 
   // Initialize Firebase
   var config = {
@@ -31,16 +31,38 @@ $("#submit").on("click", function () {
     trainData.firstTime = $('#time').val()
     trainData.freqMin=$('#frequency').val()
 
+    //create local variables 
+var newTrain ={
+    name=name,
+    destination=destination, 
+    firstTime=firstTime,
+    freqMin=freqMin,
+    nextArrival=nextArrival,
+    minutesAway=minutesAway,
+};
+database.ref().push(newTrain)
+
+  // Clears all of the text-boxes
+  $("#name").val("");
+  $("#destination").val("");
+  $("#time").val("");
+  $("#frequency").val("");
+});
+
+database.ref().on("child_added", function (childSnapshot) {
+    $('#tableContents').append('<tr>' + '<td>' + childSnapshot.val().name + '<td>' + childSnapshot.val().destination + '<td>' + childSnapshot.val().firstTime + '<td>' + childSnapshot.val().freqMin + '<td>' + childSnapshot.val().nextArrival)
+
+
 //get time of first train
-    var firstTrainTime = getTime(firstTime, "hh:mm");
+    var firstTrainTime = moment(firstTime, "hh:mm");
 	    console.log(firstTrainTime);
 
 //get current time
-    var currentTime = now.getTime();
+    var currentTime = moment();
         console.log("CURRENT TIME: " + currentTime.format("hh:mm"));
 
 //get difference between current time and first train
-    var diffInTime = differenceInMinutes((firstTrainTime), "minutes");
+    var diffInTime = moment().diff(moment(firstTrainTime), "minutes");
         console.log("DIFFERENCE IN TIME: " + diffInTime);
  
 //divide the difference in time in minutes by freq of train
@@ -52,25 +74,20 @@ var minutesLeft=freqMin - remainder;
     console.log(minutesLeft);
 
 //figure out next train
-var nextTrain = getTime().add(minutesLeft, "minutes").format("hh:mm");
-    console.log("Next Train Arrival Time: "+ nextTrain.format("hh:mm"));
+var nextTrain = moment().add(minutesLeft, "minutes").format("hh:mm");
+    console.log("Next Train Arrival Time: "+ moment(nextTrain).format("hh:mm"));
 
-//create local variables 
-var newTrain ={
-    name=name,
-    destination=destination, 
-    firstTime=firstTime,
-    freqMin=freqMin,
-    nextArrival=nextArrival,
-    minutesAway=minutesAway,
-}
-});
-    database.ref().push(trainData)
+$("#tableContents").append("<tr><td>" + "</td><td>" + name + "</td><td>" + destination + "</td><td>" +
+freqMin + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
+
+}, function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
 
 
 
 
-database.ref().on("child_added", function (childSnapshot) {
-    $('#tableContents').append('<tr>' + '<td>' + childSnapshot.val().name + '<td>' + childSnapshot.val().destination + '<td>' + childSnapshot.val().firstTime + '<td>' + childSnapshot.val().freqMin + '<td>' + childSnapshot.val().nextArrival)
 
-})
+
+
+
